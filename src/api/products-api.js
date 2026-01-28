39b5e4705c493c324d3d676360b708f88390fe59
hugopracticas <hugoloyolamaya@gmail.com>
2026-01-27 22:06:28 -0600

    Update url from aws

--- src/api/products-api.js
+++ src/api/products-api.js
@@ -5,7 +5,8 @@ export const getProducts = async() => {
     try {
         const response = await axios({
             //url: 'http://localhost:8080/api/products',
-            url: 'http://3.23.82.119:8080/api/products',
+            //url: 'http://3.23.82.119:8080/api/products',
+            url: 'http://3.23.82.119:3000/api/products',
             method: 'GET'
         })
         if(response.status !== 200){
